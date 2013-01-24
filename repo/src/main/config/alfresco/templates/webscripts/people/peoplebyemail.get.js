9f8f51580d50da94687b6eb90e3f846525391d8a
Niklas Ekman <niklas.ekman@gmail.com>
2013-01-24 11:04:15 +0100

    Added client side check for existing user when external user inviting
    Signed-off-by: Niklas Ekman <niklas.ekman@gmail.com>

---
+++ repo/src/main/config/alfresco/templates/webscripts/people/peoplebyemail.get.js
@@ -0,0 +1,22 @@
+function main() {
+   // Get the args
+   var email = args["email"];
+
+   model.peoplelist = [];
+
+   if (!email || email == "") {
+      return;
+   }
+
+   var query = "TYPE:\"cm:person\" AND email:'" + email + "'";
+
+   var def = {
+      query: query,
+      store: "workspace://SpacesStore",
+      language: "fts-alfresco"
+   };
+
+   model.peoplelist = search.query(def);
+}
+
+main();
