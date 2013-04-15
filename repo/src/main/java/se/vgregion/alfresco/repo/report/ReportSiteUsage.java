9cee9cb30b6021d8e83f911f7e4d3b880431bac2
Niklas Ekman <niklas.ekman@gmail.com>
2013-04-15 10:05:30 +0200

    Fixed some minor issues with spring injection and the user statistics feature
    Signed-off-by: Niklas Ekman <niklas.ekman@gmail.com>

--- repo/src/main/java/se/vgregion/alfresco/repo/report/ReportSiteUsage.java
+++ repo/src/main/java/se/vgregion/alfresco/repo/report/ReportSiteUsage.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.activities.ActivityService;
 import org.alfresco.service.cmr.model.FileFolderService;
 import org.alfresco.service.cmr.model.FileInfo;
@@ -17,32 +16,52 @@ import org.alfresco.service.cmr.site.SiteService;
 import org.alfresco.util.ISO8601DateFormat;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
 
-public class ReportSiteUsage {
+public class ReportSiteUsage implements InitializingBean {
 
-  private static ServiceRegistry serviceRegistry;
-  private static ActivityService activityService;
+  private ActivityService _activityService;
+
+  private NodeService _nodeService;
+
+  private FileFolderService _fileFolderService;
+
+  private SiteService _siteService;
+
   public static final String DOCUMENT_LIBRARY = "documentLibrary";
+
+  public void setActivityService(ActivityService activityService) {
+    _activityService = activityService;
+  }
+
+  public void setNodeService(NodeService nodeService) {
+    _nodeService = nodeService;
+  }
+
+  public void setFileFolderService(FileFolderService fileFolderService) {
+    _fileFolderService = fileFolderService;
+  }
+
+  public void setSiteService(SiteService siteService) {
+    _siteService = siteService;
+  }
 
   /**
    * Get the total size for all files and folders in a site in bytes
-   *
+   * 
    * @param siteNodeRef
    * @return
    */
   public long getSiteSize(NodeRef siteNodeRef) {
-    NodeService nodeService = serviceRegistry.getNodeService();
-
-    FileFolderService fileFolderService = serviceRegistry.getFileFolderService();
-
     long size = 0;
 
-    if (nodeService.exists(siteNodeRef)) {
-      NodeRef dlNodeRef = fileFolderService.searchSimple(siteNodeRef, DOCUMENT_LIBRARY);
+    if (_nodeService.exists(siteNodeRef)) {
+      NodeRef dlNodeRef = _fileFolderService.searchSimple(siteNodeRef, DOCUMENT_LIBRARY);
 
-      if (nodeService.exists(dlNodeRef)) {
+      if (_nodeService.exists(dlNodeRef)) {
         // List files in document library root and calculate size
-        List<FileInfo> listFiles = fileFolderService.listFiles(dlNodeRef);
+        List<FileInfo> listFiles = _fileFolderService.listFiles(dlNodeRef);
 
         for (FileInfo fileInfo : listFiles) {
           ContentData contentData = fileInfo.getContentData();
@@ -55,12 +74,12 @@ public class ReportSiteUsage {
         }
 
         // List all folders and calculate site for all files
-        List<FileInfo> allFolders = fileFolderService.listDeepFolders(dlNodeRef, null);
+        List<FileInfo> allFolders = _fileFolderService.listDeepFolders(dlNodeRef, null);
 
         for (FileInfo folderInfo : allFolders) {
           NodeRef folderNodeRef = folderInfo.getNodeRef();
 
-          listFiles = fileFolderService.listFiles(folderNodeRef);
+          listFiles = _fileFolderService.listFiles(folderNodeRef);
 
           for (FileInfo fileInfo : listFiles) {
             ContentData contentData = fileInfo.getContentData();
@@ -82,67 +101,61 @@ public class ReportSiteUsage {
 
   /**
    * Get the number of members of a site
-   *
+   * 
    * @param site
    * @return
    */
   public int getNumberOfSiteMembers(SiteInfo site) {
-    NodeService nodeService = serviceRegistry.getNodeService();
-    SiteService siteService = serviceRegistry.getSiteService();
-    if (nodeService.exists(site.getNodeRef())) {
-      Map<String, String> listMembers = siteService.listMembers(
-          site.getShortName(), null, null, 0, true);
+    if (_nodeService.exists(site.getNodeRef())) {
+      Map<String, String> listMembers = _siteService.listMembers(site.getShortName(), null, null, 0, true);
+
       return listMembers.size();
     } else {
-      throw new InvalidNodeRefException("Site could not be found",
-          site.getNodeRef());
+      throw new InvalidNodeRefException("Site could not be found", site.getNodeRef());
     }
   }
 
   /**
    * Get last activity on site (date)
-   *
+   * 
    * @param siteNodeRef
    * @return
    * @throws Exception
    */
   public Date getLastActivityOnSite(SiteInfo siteInfo) throws Exception {
-    List<String> siteFeedEntries = activityService.getSiteFeedEntries(
-        siteInfo.getShortName(), "json");
+    List<String> siteFeedEntries = _activityService.getSiteFeedEntries(siteInfo.getShortName(), "json");
+
     JSONParser p = new JSONParser();
 
     if (siteFeedEntries.size() == 0) {
       return null;
     } else {
       Date lastDate = null;
+
       for (String feedEntry : siteFeedEntries) {
         JSONObject parsedObject = (JSONObject) p.parse(feedEntry);
+
         if (parsedObject != null) {
           String stringDate = (String) parsedObject.get("postDate");
+
           Date entryDate = ISO8601DateFormat.parse(stringDate);
+
           if (lastDate == null || entryDate.compareTo(lastDate) > 0) {
             lastDate = entryDate;
           }
         }
       }
+
       return lastDate;
     }
   }
 
-  public ServiceRegistry getServiceRegistry() {
-    return ReportSiteUsage.serviceRegistry;
-  }
-
-  public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-    ReportSiteUsage.serviceRegistry = serviceRegistry;
-  }
-
-  public ActivityService getActivityService() {
-    return activityService;
-  }
-
-  public void setActivityService(ActivityService activityService) {
-    ReportSiteUsage.activityService = activityService;
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    Assert.notNull(_activityService);
+    Assert.notNull(_fileFolderService);
+    Assert.notNull(_nodeService);
+    Assert.notNull(_siteService);
   }
 
 }
--- repo/src/main/java/se/vgregion/alfresco/repo/scripts/ActiveUsersReport.java
+++ repo/src/main/java/se/vgregion/alfresco/repo/scripts/ActiveUsersReport.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.alfresco.service.ServiceRegistry;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.DeclarativeWebScript;
 import org.springframework.extensions.webscripts.Status;
@@ -15,21 +14,23 @@ import se.vgregion.alfresco.repo.report.UserLoginDetails;
 
 public class ActiveUsersReport extends DeclarativeWebScript {
 
-  private static ServiceRegistry serviceRegistry;
+  private ActiveUsers _activeUsers;
+
+  public void setActiveUsers(ActiveUsers activeUsers) {
+    _activeUsers = activeUsers;
+  }
 
   @Override
-  protected Map<String, Object> executeImpl(final WebScriptRequest req,
-      final Status status, final Cache cache) {
+  protected Map<String, Object> executeImpl(final WebScriptRequest req, final Status status, final Cache cache) {
     Map<String, Object> model = new HashMap<String, Object>();
 
-    ActiveUsers au = new ActiveUsers();
     Map<String, List<UserLoginDetails>> activeUsersByZone;
     try {
-      activeUsersByZone = au.getActiveUsersByZone();
-
-    } catch(Exception e) {
+      activeUsersByZone = _activeUsers.getActiveUsersByZone();
+    } catch (Exception e) {
       throw new RuntimeException(e);
     }
+
     int size = activeUsersByZone.get(ActiveUsers.INTERNAL_USERS).size();
     Map<String, Object> internalModel = new HashMap<String, Object>();
 
@@ -50,48 +51,32 @@ public class ActiveUsersReport extends DeclarativeWebScript {
 
     model.put(ActiveUsers.INTERNAL_USERS, internalModel);
     model.put(ActiveUsers.EXTERNAL_USERS, externalModel);
-    /*
-		String sites = req.getParameter("sites");
 
-		ReportSiteUsage rsu = new ReportSiteUsage();
-		SiteService siteService = serviceRegistry.getSiteService();
-		List<SiteInfo> allSites = siteService.listSites(null, null);
-		List<Map<String, Serializable>> sitesResult = new ArrayList<Map<String, Serializable>>();
-		try {
-			for (SiteInfo site : allSites) {
-				NodeRef nodeRef = site.getNodeRef();
-				long siteSize = rsu.getSiteSize(nodeRef);
-				long siteMembers = rsu.getNumberOfSiteMembers(site);
-				Date lastActivity = rsu.getLastActivityOnSite(site);
-				Map<String, Serializable> siteMap = new HashMap<String, Serializable>();
-				siteMap.put("shortName", site.getShortName());
-				siteMap.put("title", site.getTitle());
-				siteMap.put("size", siteSize);
-				siteMap.put("members", siteMembers);
-				DateFormat df = new SimpleDateFormat();
-				if (lastActivity != null) {
-					siteMap.put("lastActivity", df.format(lastActivity));
-				} else {
-					siteMap.put("lastActivity", "");
-				}
-				sitesResult.add(siteMap);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		model.put("recordsReturned", sitesResult.size());
-		model.put("totalRecords", sitesResult.size());
-		model.put("startIndex", 0);
-		model.put("pageSize", sitesResult.size());
-		model.put("sites", sitesResult);*/
+    /*
+     * String sites = req.getParameter("sites");
+     * 
+     * ReportSiteUsage rsu = new ReportSiteUsage(); SiteService siteService =
+     * serviceRegistry.getSiteService(); List<SiteInfo> allSites =
+     * siteService.listSites(null, null); List<Map<String, Serializable>>
+     * sitesResult = new ArrayList<Map<String, Serializable>>(); try { for
+     * (SiteInfo site : allSites) { NodeRef nodeRef = site.getNodeRef(); long
+     * siteSize = rsu.getSiteSize(nodeRef); long siteMembers =
+     * rsu.getNumberOfSiteMembers(site); Date lastActivity =
+     * rsu.getLastActivityOnSite(site); Map<String, Serializable> siteMap = new
+     * HashMap<String, Serializable>(); siteMap.put("shortName",
+     * site.getShortName()); siteMap.put("title", site.getTitle());
+     * siteMap.put("size", siteSize); siteMap.put("members", siteMembers);
+     * DateFormat df = new SimpleDateFormat(); if (lastActivity != null) {
+     * siteMap.put("lastActivity", df.format(lastActivity)); } else {
+     * siteMap.put("lastActivity", ""); } sitesResult.add(siteMap); } } catch
+     * (Exception e) { throw new RuntimeException(e); }
+     * model.put("recordsReturned", sitesResult.size());
+     * model.put("totalRecords", sitesResult.size()); model.put("startIndex",
+     * 0); model.put("pageSize", sitesResult.size()); model.put("sites",
+     * sitesResult);
+     */
+
     return model;
   }
 
-  public ServiceRegistry getServiceRegistry() {
-    return ActiveUsersReport.serviceRegistry;
-  }
-
-  public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-    ActiveUsersReport.serviceRegistry = serviceRegistry;
-  }
 }
