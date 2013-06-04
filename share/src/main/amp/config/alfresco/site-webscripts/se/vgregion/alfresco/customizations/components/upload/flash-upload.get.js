6677c7b0c8a961520687e20670cb54dcc99d54c8
Niklas Ekman <niklas.ekman@gmail.com>
2013-06-04 16:19:30 +0200

    Fixed so that documents uploaded via the repository (archive) in Share does not get the vgr:document type
    Signed-off-by: Niklas Ekman <niklas.ekman@gmail.com>

--- share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/components/upload/flash-upload.get.js
+++ share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/components/upload/flash-upload.get.js
@@ -1,18 +1,18 @@
-// @overridden projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/upload/flash-upload.get.js
-
 /**
  * Custom content types
  */
-function getContentTypes()
-{
-   // TODO: Data webscript call to return list of available types
-   var contentTypes = [
-   {
-      id: "vgr:document",
-      value: "vgr_document"
-   }];
-
-   return contentTypes;
+function getContentTypes() {
+   return contentTypes = [ {
+      id : "vgr:document",
+      value : "vgr_document"
+   } ];
 }
 
-model.contentTypes = getContentTypes();
+function getRepositoryContentTypes() {
+   return contentTypes = [ {
+      id : "cm:content",
+      value : "cm_content"
+   } ];
+}
+
+model.contentTypes = page.id == 'repository' ? getRepositoryContentTypes() : getContentTypes();
