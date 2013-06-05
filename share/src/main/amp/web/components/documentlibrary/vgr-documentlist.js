8624cc234f892f466ebea1b01fb9ab0625fd8702
Niklas Ekman <niklas.ekman@gmail.com>
2013-06-05 11:26:07 +0200

    Fixed an obscure error what occurred on the document library list
    Signed-off-by: Niklas Ekman <niklas.ekman@gmail.com>

--- share/src/main/amp/web/components/documentlibrary/vgr-documentlist.js
+++ share/src/main/amp/web/components/documentlibrary/vgr-documentlist.js
@@ -36,7 +36,7 @@
 
    Alfresco.DocumentList.prototype.onActionEditOffline = function(record) {
       var self = this;
-      
+
       if (record.node.size == 0) {
          Alfresco.util.PopupManager.displayMessage({
             text : this.msg("message.edit-offline.nocontent", record.displayName)
@@ -46,19 +46,35 @@
       }
 
       Alfresco.util.PopupManager.displayPrompt({
-         title: this.msg("message.edit-offline.observe"),
-         text: this.msg("message.edit-offline.observe.information"),
-         modal: true,
-         buttons: [{
-            text: self.msg("button.ok"),
-            handler: function () {
+         title : this.msg("message.edit-offline.observe"),
+         text : this.msg("message.edit-offline.observe.information"),
+         modal : true,
+         buttons : [ {
+            text : self.msg("button.ok"),
+            handler : function() {
                this.destroy();
-               
+
                onActionEditOffline.call(self, record);
             },
-            isDefault: true
-         }]
+            isDefault : true
+         } ]
       });
    };
 
 }(Alfresco.DocumentList.prototype.onActionEditOffline));
+
+(function(_resizeRowContainers) {
+
+   Alfresco.DocumentList.prototype._resizeRowContainers = function() {
+      var fpanel = Dom.get("alf-filters"), offset = (fpanel ? parseInt(fpanel.style.width, 10) : 160) + 390, width = (Dom.getViewportWidth() - offset) + "px", nodes = YAHOO.util.Selector.query(
+               'h3.filename', this.id + "-documents");
+      for ( var i = 0; i < nodes.length; i++) {
+         if (width == 'NaNpx') {
+            width = 'auto';
+         }
+         
+         nodes[i].parentNode.style.width = width;
+      }
+   };
+
+}(Alfresco.DocumentList.prototype._resizeRowContainers));
