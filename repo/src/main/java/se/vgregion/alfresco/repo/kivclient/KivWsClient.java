0f11afd7647821547861c5726735ffc58f4e1110
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2013-10-30 09:46:33 +0100

    Initial work for parsing vgrstruktur from kiv
    Signed-off-by: Marcus Svensson <marcus.svensson@redpill-linpro.com>

--- repo/src/main/java/se/vgregion/alfresco/repo/kivclient/KivWsClient.java
+++ repo/src/main/java/se/vgregion/alfresco/repo/kivclient/KivWsClient.java
@@ -1,10 +1,7 @@
-package se.vgregion.alfresco.kivclient;
+package se.vgregion.alfresco.repo.kivclient;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Iterator;
@@ -13,15 +10,16 @@ import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import org.alfresco.error.AlfrescoRuntimeException;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.transport.http.CommonsHttpMessageSender;
 
 import se.vgregion.ws.objects.ArrayOfPerson;
 import se.vgregion.ws.objects.Employment;
@@ -30,57 +28,64 @@ import se.vgregion.ws.services.SearchPersonEmploymentResponse;
 import se.vgregion.ws.services.String2ArrayOfAnyTypeMap.Entry;
 
 public class KivWsClient {
-  private static final String MESSAGE =
-      "<message xmlns=\"http://tempuri.org\">Hello World</message>";
 
-  private final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+  private static final String ORGANIZATION_DN = "vgrstrukturperson";
+  private static final String OU = "ou";
+
+  private WebServiceTemplate webServiceTemplate;
   private static final Logger LOG = Logger.getLogger(KivWsClient.class);
 
-  
-  public void setEndpointUri(String uri) {
-      webServiceTemplate.setDefaultUri(uri);
+  public KivWsClient(String username, String password) {
+    webServiceTemplate = new WebServiceTemplate();
+    CommonsHttpMessageSender sender = new CommonsHttpMessageSender();
+    sender.setCredentials(new UsernamePasswordCredentials(username, password));
+    webServiceTemplate.setMessageSender(sender);
   }
   
+  public void setEndpointUri(String uri) {
+    webServiceTemplate.setDefaultUri(uri);
+  }
+
   private <T> T XmlToObject(String xml, Class<T> type) throws JAXBException {
     JAXBContext context = JAXBContext.newInstance(type);
 
     Unmarshaller m = context.createUnmarshaller();
-   
+
     StringReader is = new StringReader(xml);
     JAXBElement object = (JAXBElement) m.unmarshal(is);
-    
+
     return type.cast(object.getValue());
   }
-  
-  private String getTemplate(String uri) throws IOException {
+
+  public String getTemplate(String uri) throws IOException {
     InputStream inputStream = getClass().getClassLoader().getResourceAsStream(uri);
     StringWriter writer = new StringWriter();
     IOUtils.copy(inputStream, writer, "UTF-8");
     return writer.toString();
   }
-  
-  public String searchPersonEmployment(String vgrId) throws IOException, JAXBException {
+
+  public String searchPersonEmployment(String vgrId, String department) throws IOException, JAXBException {
     String message = getTemplate("kiv/searchPersonEmployment.gt");
     message = message.replace("{vgrId}", vgrId);
     StringWriter sw = new StringWriter();
     if (LOG.isTraceEnabled()) {
-      LOG.trace("Request: "+message);
+      LOG.trace("Request: " + message);
     }
     StreamSource source = new StreamSource(new StringReader(message));
-    StreamResult result = new StreamResult(sw);  
-    
+    StreamResult result = new StreamResult(sw);
+
     webServiceTemplate.sendSourceAndReceiveToResult(source, result);
-    
-    SearchPersonEmploymentResponse xmlToObject = XmlToObject(sw.toString(), SearchPersonEmploymentResponse.class);
-    JAXBElement<ArrayOfPerson> return1 = xmlToObject.getReturn();
+
+    SearchPersonEmploymentResponse response = XmlToObject(sw.toString(), SearchPersonEmploymentResponse.class);
+    JAXBElement<ArrayOfPerson> return1 = response.getReturn();
     List<Person> persons = return1.getValue().getPerson();
-    if (persons.size()!=1) {
-      throw new AlfrescoRuntimeException("SearchPersonEmploymentResponse: Expected: 1, Actual: "+persons.size());
+    if (persons.size() != 1) {
+      throw new AlfrescoRuntimeException("SearchPersonEmploymentResponse: Expected: 1, Actual: " + persons.size());
     }
     Person person = persons.get(0);
     List<Employment> employments = person.getEmployments().getValue().getEmployment();
     Iterator<Employment> it = employments.iterator();
-    String orgStruktur = "";
+    String organizationDN = "";
     while (it.hasNext()) {
       Employment next = it.next();
       List<Entry> entries = next.getAttributes().getValue().getEntry();
@@ -89,38 +94,36 @@ public class KivWsClient {
       String ou = "";
       while (it2.hasNext()) {
         Entry next2 = it2.next();
-        if (next2.getKey().equalsIgnoreCase("vgrstrukturperson")) {
+        if (ORGANIZATION_DN.equalsIgnoreCase(next2.getKey())) {
           vgrstrukturperson = (String) next2.getValue().getAnyType().get(0);
-        } else if (next2.getKey().equalsIgnoreCase("ou")) {
+        } else if (OU.equalsIgnoreCase(next2.getKey())) {
           ou = (String) next2.getValue().getAnyType().get(0);
         }
-        if (ou.length()>0 && vgrstrukturperson.length()>0) {
+        if (ou.length() > 0 && vgrstrukturperson.length() > 0) {
           break;
         }
       }
-      
-      if (ou.equalsIgnoreCase("Infrastruktur 6023")) {
-        orgStruktur = vgrstrukturperson;
+
+      if (department.equalsIgnoreCase(ou)) {
+        organizationDN = vgrstrukturperson;
       }
     }
     if (LOG.isTraceEnabled()) {
-      LOG.trace("Response: "+sw.toString());
+      LOG.trace("Response: " + sw.toString());
     }
-    
-    return orgStruktur;
-  }
-  // send to the configured default URI
-  public void simpleSendAndReceive() {
-      StreamSource source = new StreamSource(new StringReader(MESSAGE));
-      StreamResult result = new StreamResult(System.out);
-      webServiceTemplate.sendSourceAndReceiveToResult(source, result);
+
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Retrieved organizationDN=" + organizationDN + " for user " + vgrId);
+    }
+
+    return organizationDN;
   }
 
-  // send to an explicit URI
-  public void customSendAndReceive() {
-      StreamSource source = new StreamSource(new StringReader(MESSAGE));
-      StreamResult result = new StreamResult(System.out);
-      webServiceTemplate.sendSourceAndReceiveToResult(source, result);
+  public WebServiceTemplate getWebServiceTemplate() {
+    return webServiceTemplate;
   }
-
+  
+  public void setWebServiceTemplate(WebServiceTemplate webServiceTemplate) {
+    this.webServiceTemplate = webServiceTemplate;
+  }
 }
--- repo/src/main/java/se/vgregion/alfresco/repo/node/ExtendPersonPolicy.java
+++ repo/src/main/java/se/vgregion/alfresco/repo/node/ExtendPersonPolicy.java
@@ -31,6 +31,7 @@ import org.apache.cxf.jaxb.JAXBUtils;
 import org.apache.log4j.Logger;
 import org.springframework.util.Assert;
 
+import se.vgregion.alfresco.repo.kivclient.KivWsClient;
 import se.vgregion.alfresco.repo.model.VgrModel;
 //import se.vgregion.client.VGRegionWebServiceClient;
 import se.vgregion.ws.services.VGRException;
@@ -47,10 +48,7 @@ public class ExtendPersonPolicy extends AbstractPolicy implements OnUpdateNodePo
   private TransactionService transactionService;
   private static final String KEY_PERSON_INFO = ExtendPersonPolicy.class.getName() + ".personInfoUpdate";
   private static final Logger LOG = Logger.getLogger(ExtendPersonPolicy.class);
-
-  private String kivWsEndpoint;
-  private String kivWsUsername;
-  private String kivWsPassword;
+  private KivWsClient kivWsClient;
 
   @Override
   public void onUpdateNode(final NodeRef nodeRef) {
@@ -117,16 +115,12 @@ public class ExtendPersonPolicy extends AbstractPolicy implements OnUpdateNodePo
     this.transactionService = transactionService;
   }
 
-  public void setKivWsEndpoint(String kivWsEndpoint) {
-    this.kivWsEndpoint = kivWsEndpoint;
+  public KivWsClient getKivWsClient() {
+    return kivWsClient;
   }
 
-  public void setKivWsUsername(String kivWsUsername) {
-    this.kivWsUsername = kivWsUsername;
-  }
-
-  public void setKivWsPassword(String kivWsPassword) {
-    this.kivWsPassword = kivWsPassword;
+  public void setKivWsClient(KivWsClient kivWsClient) {
+    this.kivWsClient = kivWsClient;
   }
 
   @Override
@@ -134,9 +128,6 @@ public class ExtendPersonPolicy extends AbstractPolicy implements OnUpdateNodePo
     super.afterPropertiesSet();
 
     Assert.notNull(threadPoolExecutor);
-    Assert.notNull(kivWsEndpoint);
-    Assert.notNull(kivWsUsername);
-    Assert.notNull(kivWsPassword);
 
     _policyComponent.bindClassBehaviour(OnUpdateNodePolicy.QNAME, ContentModel.TYPE_PERSON, new JavaBehaviour(this, "onUpdateNode", NotificationFrequency.TRANSACTION_COMMIT));
 
@@ -201,75 +192,11 @@ public class ExtendPersonPolicy extends AbstractPolicy implements OnUpdateNodePo
       if (LOG.isDebugEnabled()) {
         LOG.debug("Handling person info update for " + personNodeRef);
       }
-      String hsaId = "SE2321000131-P000000118989";
-      jaxbContext = JAXBContext.newInstance(SearchPersonEmployment.class);
-      InputStream inputStream = getClass().getClassLoader().getResourceAsStream("kiv/searchPersonEmployment.gt");
-      StringWriter writer = new StringWriter();
-      IOUtils.copy(inputStream, writer, "UTF-8");
-      String theString = writer.toString();
-      Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-      Object jaxbObject = unmarshaller.unmarshal(new StringReader(theString));
-      SearchPersonEmployment se = (SearchPersonEmployment) jaxbObject;
-      String filter = "(hsaIdentity=" + (String) hsaId + ")";
-
-      LOG.info("SearchPersonEmployment filter: {}"+ filter);
-
-
-
-      //LOG.debug("SearchPersonEmployment request: {}"+ request);
-
-      //return request;
-      //VGRegionWebServiceClient kivClient = new VGRegionWebServiceClient(kivWsUsername, kivWsPassword, false);
+      String vgrId="ricde";
+      String department = "Infrastruktur 6023";
+      String searchPersonEmployment = kivWsClient.searchPersonEmployment(vgrId, department);
       
       
-      //
-      //Person personAtSpecificTime = kivClient.getPersonAtSpecificTime(hsaId, "2013-10-20");
-      //LOG.info(personAtSpecificTime.getAttributes().toString());
-      /*
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Setup connection to kiv web service: "+kivWsEndpoint+" username: "+kivWsUsername+" password"+kivWsPassword);
-      }
-      
-      //URL wsUrl = new URL(kivWsEndpoint);
-      URL wsUrl = PersonInfoUpdater.class.getResource("/wsdl/kiv-wsService.wsdl");
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Loading wsdl from: "+wsUrl);
-      }
-      //JAXBContext jaxbContext = JAXBContext.newInstance("se.vgregion.ws");
-      VGRegionWebServiceImpl kivService = new VGRegionWebServiceImpl(wsUrl);
-      VGRegionWebServiceImplPortType kivServicePort = kivService.getVGRegionWebServiceImplPort();
-      
-      
-      
-      
-      
-      
-      
-      
-      // Handle authentication begin
-      Map<String, Object> requestContext = ((BindingProvider) kivServicePort).getRequestContext();
-      Map<String, List<String>> headers = new HashMap<String, List<String>>();
-      
-      String basicAuth = "Basic ";
-      String userPwdString = kivWsUsername+":"+kivWsPassword;
-      String encodeBase64String = Base64.encodeBase64String(userPwdString.getBytes());
-      basicAuth = basicAuth + encodeBase64String;
-      headers.put("Authorization", Collections.singletonList(basicAuth));
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("Additional request headers: "+headers.toString());
-      }
-      requestContext.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
-      requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-          kivWsEndpoint);
-      // Handle authentication end
-      
-      
-      
-      
-      
-      //kivServicePort.getPersonAtSpecificTime(hsaIdentity, timestamp)
-
-      */
    }
   }
 
