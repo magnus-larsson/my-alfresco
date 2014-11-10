372fc3280e25c6a7fe0262269e52db28b1b68dad
Niklas Ekman <niklas.ekman@redpill-linpro.com>
2014-11-10 09:54:50 +0100

    Removed an anused import which fails in jdk 1.6

--- toolkit/src/main/java/se/vgregion/alfresco/toolkit/CheckSolrIndex.java
+++ toolkit/src/main/java/se/vgregion/alfresco/toolkit/CheckSolrIndex.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.service.cmr.repository.NodeRef;
