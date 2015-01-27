b6124372f9a98378e6e4b4316d2799aee74aa33d
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2015-01-27 17:06:10 +0100

    Document details uppgraderat till 4.2

--- share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/components/document-details/document-actions.get.js
+++ share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/components/document-details/document-actions.get.js
@@ -1,3 +1,5 @@
+// @overridden projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/document-details/document-actions.get.js
+
 var connector = remote.connect("alfresco");
 
 var result = connector.get("/slingshot/doclib/container/" + model.site + "/" + model.container);
