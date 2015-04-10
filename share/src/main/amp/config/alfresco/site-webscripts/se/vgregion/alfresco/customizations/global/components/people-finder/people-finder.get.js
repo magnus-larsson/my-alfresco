b0b3f3a0324db55713a78eba8b0c55d342139331
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2015-04-10 15:27:56 +0200

    Undo of change, but added comment to clarify why this change is.

--- share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/components/people-finder/people-finder.get.js
+++ share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/components/people-finder/people-finder.get.js
@@ -1,3 +1,7 @@
 // @overridden projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/people-finder/people-finder.get.js
 
-//model.widgets[0].options.dataWebScript = "vgr/people";
+/**
+Varför?
+Yes, VGR vill inte ha default-sök beteendet. Alltså söker man på "niklas ekman" i *standard* alfresco så söker den på "niklas or ekman”, de vill ha ”niklas and ekman”.
+**/
+model.widgets[0].options.dataWebScript = "vgr/people";
