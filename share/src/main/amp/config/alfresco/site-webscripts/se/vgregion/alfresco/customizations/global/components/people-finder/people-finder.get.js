12cbbb4a328fdfabf7ca5381dbd5231d252ad021
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2015-04-17 09:28:46 +0200

    Fixed VGR search for people customization to be compatible with invite page in 4.2

--- share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/components/people-finder/people-finder.get.js
+++ share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/components/people-finder/people-finder.get.js
@@ -1,7 +1,13 @@
 // @overridden projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/people-finder/people-finder.get.js
 
-/**
-Varför?
-Yes, VGR vill inte ha default-sök beteendet. Alltså söker man på "niklas ekman" i *standard* alfresco så söker den på "niklas or ekman”, de vill ha ”niklas and ekman”.
-**/
-model.widgets[0].options.dataWebScript = "vgr/people";
+function vgr_main() {
+  /**
+  * Replace all standard people serches with custom VGR script where it has not yet been customized.
+  * This customization changes the default search from "firstname or lastname" to "firstname and lastname"
+  **/
+  if (model.widgets[0].options.dataWebScript.indexOf("vgr")===-1) {
+    model.widgets[0].options.dataWebScript = "vgr/people";
+  }
+}
+
+vgr_main();
