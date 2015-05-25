49920ed9c040d804234b58ee31e8a11b2a2583c3
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2015-05-25 08:39:54 +0200

    Added feature to become site manager for a site without being a member as admin

--- share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/share/header/share-header.get.js
+++ share/src/main/amp/config/alfresco/site-webscripts/se/vgregion/alfresco/customizations/global/share/header/share-header.get.js
@@ -1,3 +1,5 @@
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js">
+
 (function () {
 
   var userMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_USER_MENU");
@@ -22,4 +24,31 @@
     userMenuWidgets.push(logoutMenu);
   }
 
+  //Show the become site manager button for admins which are not members of a site!
+  var siteConfig = widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITE_CONFIGURATION_DROPDOWN");
+  if (siteConfig != null) {
+    var siteData = getSiteData();
+    if (siteData != null) {
+      if (user.isAdmin && !siteData.userIsMember && !siteData.userIsSiteManager) {
+        // If the user is an admin, and a site member, but NOT the site manager then
+        // add the menu item to let them become a site manager...
+        siteConfig.config.widgets.push({
+          id: "HEADER_BECOME_SITE_MANAGER",
+          name: "alfresco/menus/AlfMenuItem",
+          config: {
+            id: "HEADER_BECOME_SITE_MANAGER",
+            label: "become_site_manager.label",
+            iconClass: "alf-cog-icon",
+            publishTopic: "ALF_BECOME_SITE_MANAGER",
+            publishPayload: {
+              site: page.url.templateArgs.site,
+              siteTitle: siteData.profile.title,
+              user: user.name,
+              userFullName: user.fullName
+            }
+          }
+        });
+      }
+    }
+  }
 }());
